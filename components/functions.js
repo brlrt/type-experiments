dc72875fc869bf322ff525911739dee48ab2074d
Tim Rodenbröker <post@timrodenbroeker.de>
2017-10-11 20:10:44 +0200

    some styling

--- components/functions.js
+++ components/functions.js
@@ -6,3 +6,21 @@
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
+
+// Page transitions
+
+$(document).ready(function() {
+
+    $('#stage').css('display', 'none');
+    $('#stage').fadeIn(500);
+
+    $('#navigation a').click(function() {
+        event.preventDefault();
+        newLocation = this.href;
+        $('#stage').fadeOut(500, newpage);
+    });
+
+    function newpage() {
+        window.location = newLocation;
+    }
+});
--- components/navigation.php
+++ components/navigation.php
@@ -11,12 +11,18 @@
 	//print each file name
 	foreach($files as $file)
 	{
+
+		$justTheNumber = str_replace("sketches/","",$file);
+
 		echo '<a id="link';
 		echo '"';
+		if ($_GET["file"] == $justTheNumber){
+			echo ' class="active"';
+		}
 		echo ' href="';
 		echo 'index.php?file='.str_replace("sketches/","",$file);
 		echo '">';
-		echo str_replace("sketches/","",$file);
+		echo $justTheNumber;
 		echo '</a>';
 
 		//check to see if the file is a folder/directory
